08b3d0e7ed9cc1d9d2a53a98ccd6fa3c50b5b8ea
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-21 09:28:44 +0530

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
 
 // document.write('Hello world!');
+// console.log('Hello');
+// console.log(10 + 20);
 
-console.log('Hello');
-console.log(10 + 20);
+/*
+console.log();
+console.log();
+*/
+
+// Hello.java 
+// Hello.class
+
+// HTML - renders text and media in browser 
+// CSS - style HTML 
+// JS - programming logic in HTML / interactivity in HTML 
+
+// JS Learning 
+// datatypes, declartion of variables, operators, keywords, 
+// loops, if else, functions, objects 
+// error handling 
+
+// variable declaration in JS 
+
+// java 
+// int num = 10;
+
+// JS 
+let num = 10;
+const num2 = 20;
+
+console.log(num);
+console.log(num2);
+
+
+
+
