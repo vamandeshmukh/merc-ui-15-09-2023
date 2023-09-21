7069910cdb78cc6276c1d67baee4c730aac3eb21
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-21 12:17:54 +0530

    js

--- js/script.js
+++ js/script.js
@@ -158,3 +158,131 @@ console.log();
 // addNums(10, 20); // 30 
 // addNums(10, 20, 30); // 
 
+// Function hoisting 
+
+// fun(); // works 
+
+// function fun() {
+//     console.log('fun called');
+// };
+
+// fun(); // error
+
+// const fun = () => {
+//     console.log('fun called');
+// };
+
+// Variable hoisting 
+
+// console.log(num);
+
+// num = 10;
+// var num = 10;
+// const num = 10;
+// let num = 10;
+
+
+// const empData = {
+//     eid: 101,
+//     firstName: 'Sonu',
+//     printFirstName: function () {
+//         console.log(this.firstName);
+//     }
+// };
+
+// const empData = {
+//     eid: 101,
+//     firstName: 'Sonu',
+//     printFirstName: () => {
+//         console.log(this.firstName);
+//     }
+// };
+
+// empData.printFirstName();
+
+// truthy falsy 
+// falsy - false, 0, '', undefined, null 
+// truthy - any other value 
+
+// let firstName = 1; // 
+
+// if (firstName) {
+//     console.log('truthy');
+// }
+// else {
+//     console.log('falsy');
+// }
+
+// copy by value (primitive types)
+
+// let num = 10;
+// let num2 = num;
+
+// console.log(num); // 10
+// console.log(num2); // 10
+
+// num = 15;
+
+// console.log(num); // 15 
+// console.log(num2); // 10
+
+
+// num2 = 20;
+
+// console.log(num); // 15 
+// console.log(num2); // 20
+
+// copy by reference (objects and arrays)
+
+// const empData = {
+//     firstName: 'Sonu',
+//     salary: 90000
+// };
+
+// const data2 = empData;
+
+// console.log(empData);
+// console.log(data2);
+
+// empData.firstName = 'Monu';
+
+// console.log(empData);
+// console.log(data2);
+
+// data2.salary = 'abc';
+
+// console.log(empData);
+// console.log(data2);
+
+
+// Execution context in JS 
+
+// Global execution context 
+// Function execution context 
+
+
+// function fun() {
+//     console.log('fun called');
+//     fun2();
+// };
+
+// function fun2() {
+//     console.log('fun2 called')
+// };
+
+// fun();
+
+// Special operators 
+
+// typeof 
+// delete
+// ,
+// instanceof
+// 'condition' ? val1 : val2
+
+// const num = 11;
+// const result = num == 10 ? 'yes' : 'no';
+// console.log(result);
+
+
+
