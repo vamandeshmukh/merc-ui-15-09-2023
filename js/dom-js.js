1a0739f86232199f7be1ea0cd0b24c6b4473d18f
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-25 11:58:32 +0530

    js

--- js/dom-js.js
+++ js/dom-js.js
@@ -3,10 +3,19 @@
 
 console.log('DOM manipulation using JS');
 
-const printOutput = () => {
-    const userInput = 
-    document.getElementById('username').value;
-    console.log(userInput);
-    document.getElementById('output').innerHTML = 
-    userInput;
-};
+// const printOutput = () => {
+//     const userInput = 
+//     document.getElementById('username').value;
+//     console.log(userInput);
+//     document.getElementById('output').innerHTML = 
+//     userInput;
+// };
+
+const btn = document.getElementById('printbutton');
+
+btn.addEventListener('click', () => {
+
+    const userInput = document.getElementById('username').value;
+    document.getElementById('output').innerHTML = userInput;
+
+});
