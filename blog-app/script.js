b2f9b80e5a51da0f5d52206f0efc92e2fcf0a699
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-25 15:02:04 +0530

    js

--- blog-app/script.js
+++ blog-app/script.js
@@ -1,15 +1,17 @@
 
 console.log('blog app');
 
+// home page - welcome 
+// blogs list 
+// individual blog post 
+// write new blog 
 
 // Fetch data from a service and consume it and render it on HTML page 
-
 
 const url = 'https://jsonplaceholder.typicode.com/posts/22';
 
 fetch(url)
     .then((resp) => {
-        console.log(resp);
         return resp.json();
     })
     .then((data) => {
@@ -17,7 +19,10 @@ fetch(url)
         document.getElementById('blog-title').innerHTML = data.title;
         document.getElementById('blog-body').innerHTML = data.body;
     })
-    .catch();
+    .catch((error) => {
+        console.log(error);
+    });
 
 
+// axios
 
