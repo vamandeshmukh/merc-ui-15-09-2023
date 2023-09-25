2714fbec84bdb7ed418df7c0375875e12820836b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-25 09:32:25 +0530

    js

--- js/async-js.js
+++ js/async-js.js
@@ -9,6 +9,26 @@
 
 // console.log('Three');
 
-// Functions as arguments to another functions 
+// const fun = (arg) => {
+//     const num = arg * 2;
+//     console.log(num); 
+// };
 
+// fun(10);  // anonymous 
+
+// const a = 20; // named 
+// fun(a);
+
+// Function as arguments to another function 
+// In JS, a function can take another function as argument 
+
+const fun = (arg) => {
+    console.log('fun function called');
+    arg();
+};
+
+fun(() => { console.log('anonymous function called'); }); // like line 19 
+
+const namedFunction = () => { console.log('named function called') };
+fun(namedFunction); // like line 22
 
