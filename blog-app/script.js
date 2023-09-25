adefe8e933b38857e3161402492944498761c903
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-25 14:24:01 +0530

    js

---
+++ blog-app/script.js
@@ -0,0 +1,13 @@
+
+console.log('blog app');
+
+
+// document.getElementById('blog-title').innerHTML = 'Sample Title';
+
+
+// Fetch data from a service and consume it and render it on HTML page 
+
+
+const url = 'https://jsonplaceholder.typicode.com/posts/22';
+
+fetch()
