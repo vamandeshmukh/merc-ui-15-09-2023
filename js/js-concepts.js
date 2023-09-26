bce23a48d0235eee275efea82853e328695f6257
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-26 09:53:24 +0530

    js

---
+++ js/js-concepts.js
@@ -0,0 +1,94 @@
+// Strict mode 
+// ES5 feature - 
+
+// "use strict";
+
+// // num = 10;
+// var num2 = 20;
+
+// const num3 = 30;
+// let num4 = 40;
+
+// // console.log(num);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+// var empData = {
+//     firstName: 'Sonu',
+//     firstName: 'Monu'
+// };
+
+// console.log(empData.firstName);
+
+// undefined = 10;
+// console.log(undefined);
+
+// var num = 0123;
+// console.log(num);
+
+// "use strict";
+
+// const fun = () => {
+//     console.log(this);
+// };
+
+// function fun() {
+//     console.log(this);
+// };
+
+// fun();
+
+// const empData = {
+//     firstName: 'Sonu',
+//     lastName: 'Monu',
+//     printName: function () {
+//         console.log(this.firstName);
+//     }
+// };
+
+// empData.printName();
+
+
+// const fun = (arg) => {
+//     console.log(arg.username.value);
+
+
+// };
+
+
+// const onch = (arg) => {
+//     console.log(arg.value);
+// };
+// // Events in HTML used in JS 
+
+// // click 
+
+// onclick
+// onsubmit
+// onblur
+// onreset
+// onabort
+// onload
+// onmouseenter
+// onmouseleave
+// onmouseover
+
+
+// parseInt, parseFloat 
+
+const num = 'abc';
+
+const result = parseInt(num);
+
+console.log(result);
+
+
+
+
+
+
+
+
+
+
