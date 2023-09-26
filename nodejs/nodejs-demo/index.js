320a0672efee26e30a5a03331e14bfeb0e75d5c9
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-26 14:56:32 +0530

    node

--- nodejs/nodejs-demo/index.js
+++ nodejs/nodejs-demo/index.js
@@ -4,6 +4,7 @@
 
 // edit package.json 
 // create other .js files 
+// npm install 
 
 // start the app  
 // npm start 
@@ -11,16 +12,25 @@
 // stop the app 
 // Ctrl + C 
 
-
+// import express from 'express';
 const express = require('express');
 const app = express();
+
 const port = 3000;
 
+// app.listen(arg1, arg2);
+// app.listen(port, () => { });
 
+app.listen(port, () => {
+    console.log(`App is running on ${port}.`);
+});
 
+// app.get(arg1, arg2);
+// app.get('', () => { });
 
-
-
-
+app.get('', (req, res) => {
+    console.log('welcome');
+    res.send('Welcome to Merc App');
+});
 
 
