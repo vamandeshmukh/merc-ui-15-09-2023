c4ad198fe076e76029a5d1994faf050506df74fd
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-26 14:26:28 +0530

    node

--- nodejs/nodejs-demo/index.js
+++ nodejs/nodejs-demo/index.js
@@ -1,2 +1,16 @@
+// create a new project -
+// in the project folder in CMD - 
+// npm init -y
+
+// edit package.json 
+// create other .js files 
+
+// start the app  
+// npm start 
+
+// stop the app 
+// Ctrl + C 
+
 
 console.log('start');
+
