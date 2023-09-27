017bae1b114b03484d8d89d77244c1860bccb789
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-27 15:10:40 +0530

    node

--- nodejs/blog-server/blog-data.js
+++ nodejs/blog-server/blog-data.js
@@ -3,6 +3,6 @@
 // array to store data 
 
 export const blogData = [
-    { id: 1, title: 'Happy Days', body: 'College days are happy days.', writerId: 101 },
-    { id: 2, title: 'Work Life', body: 'Work life balance is important.', writerId: 102 }
+    { id: 1, title: 'Happy Days', body: 'College days are happy days.', writerId: 1 },
+    { id: 2, title: 'Work Life', body: 'Work life balance is important.', writerId: 2 }
 ]; 
