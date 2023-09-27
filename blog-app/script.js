f07d6b4eee5c968f3bb7069b37a84d7c370e705c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-27 14:45:07 +0530

    node

--- blog-app/script.js
+++ blog-app/script.js
@@ -18,11 +18,17 @@ console.log('blog app');
 
 // Fetch data from a service and consume it and render it on HTML page 
 
-const url = 'https://jsonplaceholder.typicode.com/posts/';
+
+// const blogUrl = 'https://jsonplaceholder.typicode.com/posts/';
+
+const blogUrl = 'http://localhost:1234';
+
+const commentsUrl = 'https://jsonplaceholder.typicode.com/comments/';
+const writersUrl = 'https://jsonplaceholder.typicode.com/users/';
 
 // fetch all blogs list 
 
-fetch(url)
+fetch(`${blogUrl}/get-all-blogs`)
     .then((resp) => {
         return resp.json();
     })
@@ -30,8 +36,7 @@ fetch(url)
         console.log(data);
         let blogList = '';
         data.forEach(element => {
-            blogList += `<p>${element.title}</p>`;
-            blogList += `<a href="./blog-details.html?blog-id=${element.id}">${element.title}</a>`;
+            blogList += `<a href="./blog-details.html?blog-id=${element.id}">${element.title}</a><br />`;
         });
         document.getElementById('all-blogs').innerHTML = blogList;
     })
@@ -41,19 +46,95 @@ fetch(url)
 
 
 // fetch individual blog 
-fetch(url + '22')
+let blogId = new URLSearchParams(window.location.search).get('blog-id');
+
+if (!blogId)
+    blogId = Math.floor(Math.random() * 100);
+
+console.log(blogId);
+fetch(`${blogUrl}/get-blog-by-id/${blogId}`)
     .then((resp) => {
         return resp.json();
     })
     .then((data) => {
         console.log(data);
         document.getElementById('blog-title').innerHTML = data.title;
-        document.getElementById('blog-body').innerHTML = data.body;
+        let tempData = '';
+        data.title.split('').forEach((elem) => {
+            tempData += `<span>${data.body}</span>`;
+            console.log(elem);
+        })
+        document.getElementById('blog-body').innerHTML = tempData;
+
+        // fetch comments 
+
+        fetch(`${commentsUrl}?postId=${data.id}`)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                console.log(data);
+                document.getElementById('comments-count').innerHTML = `${data.length} comments:`;
+                let commentList = '';
+                data.forEach(element => {
+                    commentList += `<p>${element.name}</p><p>${element.body}</p><hr/>`;
+                });
+                document.getElementById('comment-list').innerHTML = commentList;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+        // fetch writer data  
+
+        fetch(`${writersUrl}/${data.userId}`)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then((data) => {
+                console.log(data);
+                document.getElementById('writer-details').innerHTML =
+                    `<p>Posted by: ${data.name}</p><p><a href="mailto:${data.email}">${data.email}</a></p>`;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+
+
     })
     .catch((error) => {
         console.log(error);
     });
 
+// write blog 
+
+const saveBlog = () => {
+    const blogData = {
+        title: document.getElementById('title').value,
+        body: document.getElementById('body').value
+    };
+
+    console.log(blogData);
+
+    fetch(`${blogUrl}/write-blog`, {
+        method: 'POST',
+        body: JSON.stringify(blogData),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+        .then((response) => {
+            console.log(response);
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            document.getElementById('write-blog-form').reset();
+            alert(`${data.title} published successfully!`);
+        });
+};
 
 
+
+// writer-details
 
--- nodejs/blog-server/index.js
+++ nodejs/blog-server/index.js
@@ -1,5 +1,7 @@
 
 import express from 'express';
+import { blogData } from './blog-data.js';
+
 const app = express();
 const port = 1234;
 
@@ -10,20 +12,32 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.get('get-blog-by-id', (req, res) => {
-    // code 
+app.get('/get-blog-by-id/:blogid', (req, res) => {
+    const blogId = req.params.blogid;
+    const blog = blogData.find(b => b.id == blogId);
+    console.log(blog);
+    if (blog)
+        res.status(200).send(blog);
+    else
+        res.status(404).send({ message: `Blog with blgoId ${blogId} not found!` });
 });
 
-app.get('get-blog-by-title', (req, res) => {
-    // code 
+app.get('/get-all-blogs', (req, res) => {
+    console.log(blogData.length);
+    if (blogData.length > 0)
+        res.status(200).send(blogData);
+    else
+        res.status(404).send({ message: `Blogs not found!` });
 });
 
-app.get('get-all-blogs', (req, res) => {
-    // code 
-});
-
-app.post('write-blog', (req, res) => {
-    // code 
+app.post('/write-blog', (req, res) => {
+    console.log(blogData);
+    const blogSize = blogData.length;
+    const newBlog = { id: blogSize + 1, title: req.body.title, body: req.body.body, writerId: req.body.writerId };
+    blogData.push(newBlog);
+    if (blogData.length === blogSize + 1)
+        res.status(201).send(newBlog);
+    console.log(blogData);
 });
 
 
