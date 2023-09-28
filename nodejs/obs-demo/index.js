a11b6b8de8ec620d08b84d5856acde91c315387b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-28 10:13:28 +0530

    node

--- nodejs/obs-demo/index.js
+++ nodejs/obs-demo/index.js
@@ -1,23 +1,56 @@
 
-import { Observable, from, of, filter, map } from 'rxjs';
+// import { Observable, from, of, filter, map } from 'rxjs';
 
-const myArr = [10, 20, 30, 40, 50];
+// const myArr = [10, 20, 30, 40, 50];
 
-const arrObs = from(myArr);
+// const arrObs = from(myArr);
 
-// arrObs.subscribe((val) => {
-//     console.log(val);
-// });
+// // arrObs.subscribe((val) => {
+// //     console.log(val);
+// // });
+
+// // arrObs
+// // .pipe()
+// // .subscribe()
+// // .unsubscribe();
 
 // arrObs
-// .pipe()
-// .subscribe()
-// .unsubscribe();
+//     .pipe(
+//         filter(val => val % 4 == 0),
+//         // code 
+//         map(val => val + 2)
+//     )
+//     .subscribe((val) => {
+//         // setTimeout(() => {
+//         console.log(val);
+//         // }, 2000);
+//     })
+//     .unsubscribe();
 
-arrObs
-    .pipe(
-        filter(val => val % 4 == 0),
-        map(val => val + 2)
-    )
-    .subscribe(res => console.log(res))
-    .unsubscribe();
+
+
+// callbacks 
+
+console.log('start');
+
+const asyncFun = (arg) => {
+    setTimeout(() => {
+        arg(' async ops completed');
+    }, 2000);
+};
+
+console.log('async ops started');
+
+asyncFun((result) => {
+    console.log(result);
+});
+
+console.log('end');
+
+
+
+
+
+
+
+
