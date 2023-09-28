a8c56ffc6d35773e1468ff65c4918efca0aebb82
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-28 09:40:05 +0530

    node

--- nodejs/obs-demo/index.js
+++ nodejs/obs-demo/index.js
@@ -1,10 +1,23 @@
 
-import { Observable, from, of } from 'rxjs';
+import { Observable, from, of, filter, map } from 'rxjs';
 
 const myArr = [10, 20, 30, 40, 50];
 
 const arrObs = from(myArr);
 
-arrObs.subscribe((val) => {
-    console.log(val);
-});
+// arrObs.subscribe((val) => {
+//     console.log(val);
+// });
+
+// arrObs
+// .pipe()
+// .subscribe()
+// .unsubscribe();
+
+arrObs
+    .pipe(
+        filter(val => val % 4 == 0),
+        map(val => val + 2)
+    )
+    .subscribe(res => console.log(res))
+    .unsubscribe();
