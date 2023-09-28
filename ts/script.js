c0fd1f0d6165d04127cbde5828008e26d8b6748b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-28 14:17:16 +0530

    ts

---
+++ ts/script.js
@@ -0,0 +1,16 @@
+
+// TypeScript 
+
+// Java 
+// double salary = 10; 
+
+// const salary = 10;
+const salary = 'abc';
+
+console.log(salary);
+
+// Hello.java -> compile -> Hello.class 
+// script.ts -> transpile -> script.js 
+
+
+// npm install typescript --location=global
