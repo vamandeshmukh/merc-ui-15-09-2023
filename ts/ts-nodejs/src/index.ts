0d27d1407ea4e1c429b2766f1b22a5f53f46a3ce
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-29 15:31:30 +0530

    ts

---
+++ ts/ts-nodejs/src/index.ts
@@ -0,0 +1,16 @@
+
+// NodeJS development with TypeScript 
+
+// tsc --init 
+// edit config file 
+// npm install express 
+// create src folder 
+// create index.js in src folder  
+// tsc 
+// node dist/index.js
+
+const fun = (arg: string) => {
+    console.log(`Hey ${arg} have fun!`);
+};
+
+fun('Sonu');
