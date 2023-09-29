84e0e43a6f7ca6d14c9fd750aa14f6ae42cb7016
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-29 10:34:07 +0530

    ts

--- ts/ts-classes.ts
+++ ts/ts-classes.ts
@@ -1,59 +1,106 @@
 
-console.log('ts-classes.ts');
+// console.log('ts-classes.ts');
 
-// // TS / JS Object 
-// const empData = { eid: number = 101, firstName: string = 'Sonu' };
+// // // TS / JS Object 
+// // const empData = { eid: number = 101, firstName: string = 'Sonu' };
 
-// class definition 
+// // class definition 
+// class Book {
+
+//     // properties / fields 
+//     name: string;
+//     author: string;
+//     public isbn: string;
+//     private publisher: string;
+//     protected sellPrice: number;
+
+//     // constructor 
+//     constructor(name: string, author: string) {
+//         this.name = name;
+//         this.author = author;
+
+//     }
+//     // methods 
+//     show() {
+//         console.log(`Book name: ${this.name}`);
+//         console.log(`Author: ${this.author}`);
+//     }
+// }
+// const book1 = new Book('Happy Days', 'Sonu Singh');
+// book1.show();
+// const book2 = new Book();
+// book2.show();
+
+// class Fiction extends Book {
+
+//     price: number;
+
+//     constructor(name: string, author: string, price: number) {
+//         super(name, author);
+//         this.price = price;
+//     }
+
+//     show() {
+//         console.log(`Book name: ${this.name}`);
+//         console.log(`Author: ${this.author}`);
+//         console.log(`Price ${this.price}`);
+//     }
+// }
+
+// const fictionBook = new Fiction('The New India', 'Some Writer', 999);
+// fictionBook.show();
+
+// class NonFiction extends Book {
+
+// }
+
+// const nonFictionBook = new NonFiction('Some Book Title', 'Some Other Writer');
+
+// nonFictionBook.show();
+
+
 class Book {
-
-    // properties / fields 
-    name: string;
-    author: string;
-
-    // constructor 
-    constructor(name: string, author: string) {
-        this.name = name;
-        this.author = author;
-
-    }
-    // methods 
-    show() {
-        console.log(`Book name: ${this.name}`);
-        console.log(`Author: ${this.author}`);
-    }
-}
-const book1 = new Book('Happy Days', 'Sonu Singh');
-book1.show();
-const book2 = new Book();
-book2.show();
-
-class Fiction extends Book {
-
-    price: number;
-
-    constructor(name: string, author: string, price: number) {
-        super(name, author);
-        this.price = price;
-    }
-
-    show() {
-        console.log(`Book name: ${this.name}`);
-        console.log(`Author: ${this.author}`);
-        console.log(`Price ${this.price}`);
-    }
+    public title: string;
+    public author: string;
+    public static publisher: string;
 }
 
-const fictionBook = new Fiction('The New India', 'Some Writer', 999);
-fictionBook.show();
+const book1 = new Book();
 
-class NonFiction extends Book {
+book1.author
 
+Book.publisher
+
+
+
+// abstract
+
+const addNums = (a: number, b: number): number =>  // what ?
+{
+    return a + b; // how?
+};
+
+
+abstract class Employee {
+
+    writeCode(): void {
+        console.log('Write code properly...');
+    };
+
+    abstract writeDocs(): void;
 }
 
-const nonFictionBook = new NonFiction('Some Book Title', 'Some Other Writer');
+class ContractualEmployee extends Employee {
 
-nonFictionBook.show();
+    writeDocs(): void {
+        console.log('Write documentaiton properly');
+    }
+}
+
+const obj = new ContractualEmployee();
+
+obj.writeDocs();
+
 
 
 
