0469e118167c38ce11f8ec8d8b5044374244f172
Vaman Deshmukh <vamandeshmukh@gmail.com>
2023-09-29 09:41:38 +0530

    ts

--- ts/ts-classes.js
+++ ts/ts-classes.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log('ts-classes.ts');
 // // TS / JS Object 
 // const empData = { eid: number = 101, firstName: string = 'Sonu' };
@@ -19,3 +34,28 @@ var book1 = new Book('Happy Days', 'Sonu Singh');
 book1.show();
 var book2 = new Book();
 book2.show();
+var Fiction = /** @class */ (function (_super) {
+    __extends(Fiction, _super);
+    function Fiction(name, author, price) {
+        var _this = _super.call(this, name, author) || this;
+        _this.price = price;
+        return _this;
+    }
+    Fiction.prototype.show = function () {
+        console.log("Book name: ".concat(this.name));
+        console.log("Author: ".concat(this.author));
+        console.log("Price ".concat(this.price));
+    };
+    return Fiction;
+}(Book));
+var fictionBook = new Fiction('The New India', 'Some Writer', 999);
+fictionBook.show();
+var NonFiction = /** @class */ (function (_super) {
+    __extends(NonFiction, _super);
+    function NonFiction() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return NonFiction;
+}(Book));
+var nonFictionBook = new NonFiction('Some Book Title', 'Some Other Writer');
+nonFictionBook.show();
--- ts/ts-classes.ts
+++ ts/ts-classes.ts
@@ -15,20 +15,46 @@ class Book {
     constructor(name: string, author: string) {
         this.name = name;
         this.author = author;
-    }
 
+    }
     // methods 
     show() {
         console.log(`Book name: ${this.name}`);
         console.log(`Author: ${this.author}`);
     }
 }
-
 const book1 = new Book('Happy Days', 'Sonu Singh');
 book1.show();
-
 const book2 = new Book();
 book2.show();
+
+class Fiction extends Book {
+
+    price: number;
+
+    constructor(name: string, author: string, price: number) {
+        super(name, author);
+        this.price = price;
+    }
+
+    show() {
+        console.log(`Book name: ${this.name}`);
+        console.log(`Author: ${this.author}`);
+        console.log(`Price ${this.price}`);
+    }
+}
+
+const fictionBook = new Fiction('The New India', 'Some Writer', 999);
+fictionBook.show();
+
+class NonFiction extends Book {
+
+}
+
+const nonFictionBook = new NonFiction('Some Book Title', 'Some Other Writer');
+
+nonFictionBook.show();
+
 
 
 
